b0fe384e5b088eaa0db14f9b3c4b8e796b198698
sciepsilon <kalinda.pride@gmail.com>
2017-06-28 12:51:13 -0400

    include independent tier annotations

--- preprocess.js
+++ preprocess.js
@@ -7,12 +7,27 @@ var parseString = require('xml2js').parseString; // or we could use simple-xml
 var xmlFileName = "C:\\xampp\\htdocs\\ETST\\elan_text_sync_tool\\elan_files\\test1.eaf";
 var jsonFileName = "C:\\Users\\Kalinda\\Documents\\MEGAsync\\Linguistics\\UTRA\\data_conversion\\test1_preprocessed.js"
 
+function swapJsonKeyValues(input) {
+    var output = {};
+    for (var value in input) {
+		if (input.hasOwnProperty(value)) {
+			output[input[value]] = value;
+		}
+    }
+    return output;
+}
+
 fs.readFile(xmlFileName, function (err, xml) {
   if (err) throw err;
-  console.log(xml);
-
+  
   parseString(xml, function (err, jsonIn) {
-	console.dir(jsonIn);
+	
+	var timeslotsIn = jsonIn.ANNOTATION_DOCUMENT.TIME_ORDER[0].TIME_SLOT;
+	var timeslots = [];
+	for (var slot of timeslotsIn) {
+		timeslots[slot.$.TIME_SLOT_ID] = slot.$.TIME_VALUE;
+	}
+	
 	
 	var jsonOut = 
 	{"metadata": 
@@ -22,31 +37,55 @@ fs.readFile(xmlFileName, function (err, xml) {
 	"speakers": {}
 	};
 	
-	tiers = jsonIn.ANNOTATION_DOCUMENT.TIER
-	tierNames = tiers.map((tier) => tier.$.TIER_ID);
-	for (i = 0; i < tierNames.length; i++) {
+	var tiersIncludeEmpty = jsonIn.ANNOTATION_DOCUMENT.TIER
+	// discard tiers that have no annotations in them
+	var tiers = tiersIncludeEmpty.filter((tier) => 
+			tier.ANNOTATION != null && tier.ANNOTATION.length > 0);
+	
+	var tierNames = tiers.map((tier) => tier.$.TIER_ID);
+	for (var i = 0; i < tierNames.length; i++) {
 		var newID = "T" + (i + 1).toString();
+		
 		jsonOut.metadata.tierIDs[newID] = tierNames[i];
 	}
 	
-	indepTiers = tiers.filter((tier) => tier.$.PARENT_REF == null);
-	for (i = 0; i < indepTiers.length; i++) {
+	var indepTiers = tiers.filter((tier) => tier.$.PARENT_REF == null);
+	var tierIDsFromNames = swapJsonKeyValues(jsonOut.metadata.tierIDs);
+	var annotationsFromIDs = {};
+	for (var i = 0; i < indepTiers.length; i++) {
 		var newID = "S" + (i + 1).toString();
-		tierHeader = indepTiers[i].$;
+		var tierHeader = indepTiers[i].$;
+		
 		jsonOut.metadata.speakerIDs[newID] = {
 			"name": tierHeader.PARTICIPANT,
 			"language": tierHeader.LANG_REF,
-			// "tier": tierNameToID(tierHeader.TIER_ID)
+			"tier": tierIDsFromNames[tierHeader.TIER_ID]
 			};
-	}
-	
+			
+		jsonOut.speakers[newID] = [];
 
-	prettyString = JSON.stringify(jsonOut, null, 2);
+		for (var bigAnnotation of indepTiers[i].ANNOTATION) {
+			var annotation = bigAnnotation.ALIGNABLE_ANNOTATION[0];
+			annotationsFromIDs[annotation.$.ANNOTATION_ID] = annotation;
+			jsonOut.speakers[newID].push({
+				"start_time_ms": timeslots[annotation.$.TIME_SLOT_REF1],
+				"end_time_ms": timeslots[annotation.$.TIME_SLOT_REF2],
+				/* "num_slots": get_num_slots(
+						annotation.$.TIME_SLOT_REF1, 
+						annotation.$.TIME_SLOT_REF2),
+						*/
+				"text": annotation.ANNOTATION_VALUE[0],
+				"dependents": []
+			})
+		}
+	}
+
+	var prettyString = JSON.stringify(jsonOut, null, 2);
 	fs.writeFile(jsonFileName, prettyString, function(err) {
       if(err) {
         return console.log(err);
       }
-      console.log("The file was saved!");
+      console.log("The converted file was saved. All done!");
 	}); 
   });
 });
