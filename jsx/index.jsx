00a85fd8c3f8310f73665571c918ed1ad0dbd7c9
sciepsilon <kalinda.pride@gmail.com>
2017-07-11 18:02:24 -0500

    use react data instead of fs

--- jsx/index.jsx
+++ jsx/index.jsx
@@ -1,5 +1,3 @@
-var fs = require(['fs']);
-
 class DocLink extends React.Component {
   // I/P: fileName, the filename (including file extension, excluding file path) of the document
   // O/P: a button to view that document
@@ -13,25 +11,22 @@ class DocLink extends React.Component {
 }
 
 class IndexDisplay extends React.Component {
-  // I/P: none
+  // I/P: index, a list of the JSON index metadata for each document
   // O/P: a list of buttons, one to view each document
   // Status: untested
   render () {
-    var path = "./data/json_files";
-    fs.readdir(path, function(err, fileNames) {
-      if (err) throw err;
-      
-      var output = [];
-      for (var i=0; i<fileNames.length; i++) {
-          output.push(<DocLink key={i} fileName={fileNames[i]}/>);
-      }
-    });
+    var files = this.props.data;
+    var output = [];
+    for (var i=0; i<files.length; i++) {
+      var fileName = files[i]["title from filename"];
+      output.push(<DocLink key={i} fileName={fileNames}/>);
+    }
     return <ul className="indexDisplay">{output}</ul>;
   }
 }
 
 ReactDOM.render(
-  <IndexDisplay/>,
+  <IndexDisplay data={data} />,
   document.getElementById('centerPanel')
 );
  
