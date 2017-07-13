301687ff822c483deec64f745dc386439303f377
sciepsilon <kalinda.pride@gmail.com>
2017-07-13 10:37:18 -0500

    create iso lookup table

--- preprocessing/iso_table_to_json.js
+++ preprocessing/iso_table_to_json.js
@@ -1,21 +1,25 @@
-// https://stackoverflow.com/questions/21461102/converting-html-table-to-json
-/*$('#convert-table').click( function() {
-  var table = $('#example-table').tableToJSON();
-  console.log(table);
-  alert(JSON.stringify(table));  
-});*/
-
 // Fetch a URL and parse all it's tables into JSON, using a callback 
 var tabletojson = require('tabletojson');
 var fs = require('fs');
 
 var url = 'https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes';
-var fileNameOut = "C:\\Users\\Kalinda\\Documents\\GitHub\\Korpus\\preprocessing\\all_tables.json";
+var fileNameOut = "C:\\Users\\Kalinda\\Documents\\GitHub\\Korpus\\preprocessing\\iso_dict.json";
 
 tabletojson.convertUrl(url, function(tablesAsJson) {
-  var mainTable = tablesAsJson[1]; // we want the second table on this particular page
-  // console.log(tablesAsJson);
-  var prettyString = JSON.stringify(mainTable, null, 2);
+  var mainTable = tablesAsJson[1]; // we want the second table on the Wikipedia ISO page
+  var codesDict = {};
+  for (language of mainTable) {
+    var iso1 = language["639-1"];
+    var iso2T = language["639-2/T"];
+    var iso2B = language["639-2/B"];
+    var iso3 = language["639-3"];
+    codesDict[iso1] = language;
+    codesDict[iso2T] = language;
+    codesDict[iso2B] = language;
+    codesDict[iso3] = language;
+  }
+  // now codesDict contains the same data as mainTable, but indexed by all iso codes
+  var prettyString = JSON.stringify(codesDict, null, 2);
   fs.writeFile(fileNameOut, prettyString, function(err) {
     if (err) {
       return console.log(err);
