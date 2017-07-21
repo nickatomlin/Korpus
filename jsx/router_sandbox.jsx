7a1c5a40d1434744baf26df667a042791bdb9494
sciepsilon <kalinda.pride@gmail.com>
2017-07-21 16:27:09 -0500

    basic page without Router

--- jsx/router_sandbox.jsx
+++ jsx/router_sandbox.jsx
@@ -1,4 +1,4 @@
-/*const Home = () => (
+const Home = () => (
   <div>
     <h2>Home</h2>
   </div>
@@ -10,45 +10,14 @@ const About = () => (
   </div>
 )
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)*/
-
 class BasicExample extends React.Component {
   render() {
-    return <Router><div><ul><li><Link to="/">Home</Link></li><li><Link to="/about">About</Link></li><li><Link to="/topics">Topics</Link></li></ul></div></Router>
+    console.log("rendering BasicExample");
+    return <div><ul><li>Home</li></ul></div>
   }
 }
+
+
 ReactDOM.render(
   <BasicExample/>,
   document.getElementById('centerPanel')
