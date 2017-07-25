595c105aa99998b9d70b953adb00e0b8741843b5
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2017-07-25 15:37:03 -0400

    updated title

--- jsx/sidebar/title.jsx
+++ jsx/sidebar/title.jsx
@@ -1,9 +1,6 @@
-export class TitleInfo extends React.Component {
-  // I/P: title, a string
-  // O/P: printed title
-  // Status: tested, working
-  render() {
-    var title = this.props.title;
-    return <h3 id="title">{title}</h3>;
-  }
+function Title({{ title }}) {
+	// I/P: title, a string
+	// O/P: printed title
+	// Status: tested, working
+	return <h3 id="title">{title}</h3>;
 }
