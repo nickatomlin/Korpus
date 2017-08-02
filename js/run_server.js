d42c39cd18b0bf82bbb4f5ec2e44d4c013302cac
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2017-08-02 14:54:12 -0400

    working CSS

--- js/run_server.js
+++ js/run_server.js
@@ -4,14 +4,25 @@ const fs = require('fs');
 const ReactDOMServer = require('react-dom/server');
 const serverRenderText = bundle.serverRenderText();
 
-const indexPt1 = fs.readFileSync('./Korpus/index_part1.html', 'utf8');
-const indexPt2 = fs.readFileSync('./Korpus/index_part2.html', 'utf8');
+const indexPt1 = fs.readFileSync('index_part1.html', 'utf8');
+const indexPt2 = fs.readFileSync('index_part2.html', 'utf8');
+const css = fs.readFileSync('css/main.css', 'utf8');
 
 http.createServer((req, res) => {
-    res.write(indexPt1);
-    res.write(serverRenderText);
-    res.write(indexPt2);
-    res.end();
+	if (req.url === '/' || req.url === '/js/bundle.js') {
+		res.writeHead(200, {"Content-Type": "text/html"});
+    	res.write(indexPt1);
+    	res.write(serverRenderText);
+		res.write(indexPt2);
+    	res.end();
+	} else if (req.url === '/css/main.css?version=0.2.5') {
+		res.writeHead(200, {'Content-type' : 'text/css'});
+		res.write(css);
+		res.end();
+	}
+	else {
+		console.log(req.url);
+	}
 }).listen(8000);
 
 
