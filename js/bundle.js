327b01c2e3d4d0da3fa57d6bf5f96110d920ef64
sciepsilon <kalinda.pride@gmail.com>
2017-08-02 12:38:47 -0500

    use nonempty index
    index displays correctly; css still absent; story link does nothing;
    still getting `SyntaxError: expected expression, got '<'`

--- js/bundle.js
+++ js/bundle.js
@@ -11247,7 +11247,44 @@ var _App = __webpack_require__(209);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var data = { "index": [], "stories": [] };
+var data = {
+    "index": [{
+        "title from filename": "TruncatedIntro",
+        "display_title": "Ingi Cansecho Ande (La tierra que vivimos) truncated"
+    }],
+    "stories": [{
+        "metadata": {
+            "title from filename": "Intro",
+            "tier IDs": {
+                "T1": "A'ingae",
+                "T2": "English",
+                "T3": "Morphemes",
+                "T4": "Glossed Morpheme"
+            },
+            "speaker IDs": {
+                "S1": {
+                    "name": "Martin Criollo",
+                    "tier": "T1"
+                }
+            },
+            "title": "Intro",
+            "timed": "true",
+            "media": {
+                "mp3": "Intro.mp3",
+                "mp4": "Intro.mp4"
+            }
+        },
+        "sentences": [{
+            "speaker": "S1",
+            "tier": "T1",
+            "start_time_ms": 43536,
+            "end_time_ms": 45004,
+            "num_slots": 1,
+            "text": "<- Singing",
+            "dependents": []
+        }]
+    }]
+};
 
 function Server() {
     //$.getJSON('./data/fake_database.json', function(data) {
--- jsx/server.jsx
+++ jsx/server.jsx
@@ -3,7 +3,48 @@ import { StaticRouter } from 'react-router';
 import ReactDOMServer from 'react-dom/server';
 import { App } from './App/App.jsx';
 
-const data = {"index":[], "stories":[]};
+const data =
+    {
+        "index":
+            [{
+                "title from filename": "TruncatedIntro",
+                "display_title": "Ingi Cansecho Ande (La tierra que vivimos) truncated"
+            }],
+        "stories":
+            [{
+                "metadata": {
+                    "title from filename": "Intro",
+                    "tier IDs": {
+                        "T1": "A'ingae",
+                        "T2": "English",
+                        "T3": "Morphemes",
+                        "T4": "Glossed Morpheme"
+                    },
+                    "speaker IDs": {
+                        "S1": {
+                            "name": "Martin Criollo",
+                            "tier": "T1"
+                        }
+                    },
+                    "title": "Intro",
+                    "timed": "true",
+                    "media": {
+                        "mp3": "Intro.mp3",
+                        "mp4": "Intro.mp4"
+                    }
+                },
+                "sentences":
+                    [{
+                        "speaker": "S1",
+                        "tier": "T1",
+                        "start_time_ms": 43536,
+                        "end_time_ms": 45004,
+                        "num_slots": 1,
+                        "text": "<- Singing",
+                        "dependents": []
+                    }]
+            }]
+    };
 
 function Server() {
     //$.getJSON('./data/fake_database.json', function(data) {
