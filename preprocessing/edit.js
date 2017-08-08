af88532bc4e59bc13911bb67a18d3816e5cffca5
Nicholas Tomlin <Nicholas_Tomlin@brown.edu>
2017-08-08 11:05:27 -0400

    updated index2, more null values

--- preprocessing/edit.js
+++ preprocessing/edit.js
@@ -1,4 +1,15 @@
 let fs = require('fs');
+let prompt = require('prompt');
 let obj = JSON.parse(fs.readFileSync('data/index2.json', 'utf8'));
 
-console.log(obj);
+let filename;
+
+process.argv.forEach(function (val, index, array) {
+	if (index === 2) {
+		filename = val;
+	} else if (index === 3) {
+		console.log("Too many arguments. Continuing anyway...");
+	}
+});
+
+console.log(obj[filename]);
